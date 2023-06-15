caeb1a1d70ffdeb5be8be4da9dfeb558ad286a27
gasper94 <ulimarmol94@gmail.com>
2023-06-15 12:05:06 -0700

    adding feed ui

--- components/PropmptCard.jsx
+++ components/PropmptCard.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+'use client'
 
-const PropmptCard = () => {
+import React from 'react'
+import {useState} from 'react';
+import Image from 'next/image';
+import {useSession} from 'next-auth/react';
+import {userPathname, useRouter} from 'next/navigation';
+
+const PropmptCard = ({
+  post,
+  handleTagClick,
+  handleEdit,
+  handleDelete
+}) => {
+  const [copied, setCopied] = useState('')
+
   return (
-    <div>PropmptCard</div>
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div className='flex-1 flex justifify-start items-center gap-3 cursor-pointer'>
+          <Image 
+            src={post.creator.image}
+            alt="user_image"
+            width={40}
+            height={40}
+            className='rounded-full object-contain'
+          />
+
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold'>{post.creator.username}</h3>
+            <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+          </div>
+        </div>
+
+        <div className='copy_btn' onClick={() => {}}>
+          <Image 
+            src={copied === post.prompt ?
+              '/assets/icons/tick.svg'
+            :
+              '/assets/icons/copy.svg'
+            }
+            width={12}
+            height={12}
+          />
+        </div>
+      </div>
+
+      <p className='my-4 font-satochi text-sm text-gray-700'>{post.prompt}</p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer'
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >{post.tag}</p>
+    </div>
   )
 }
 
