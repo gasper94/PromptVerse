4de3b1e1a4e345aa60c145ff312b8a2ebe0018e9
gasper94 <ulimarmol94@gmail.com>
2023-06-21 11:48:26 -0700

    updating promptcard ui for editing and deleting prompt card

--- components/PropmptCard.jsx
+++ components/PropmptCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import {useState} from 'react';
 import Image from 'next/image';
 import {useSession} from 'next-auth/react';
-import {userPathname, useRouter} from 'next/navigation';
+import {usePathname, useRouter} from 'next/navigation';
 
 const PropmptCard = ({
   post,
@@ -12,7 +12,11 @@ const PropmptCard = ({
   handleEdit,
   handleDelete
 }) => {
-  const [copied, setCopied] = useState('')
+  const { data: session } = useSession();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const [copied, setCopied] = useState('');
 
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -57,6 +61,17 @@ const PropmptCard = ({
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >{post.tag}</p>
 
+
+      {session?.user.id === post.creator._id && pathname === '/profile' && (
+        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
+          <p className='font-inter text-sm green_gradient cursor-pointer' onClick={handleEdit}>
+            Edit
+          </p>
+          <p className='font-inter text-sm orange_gradient cursor-pointer' onClick={handleEdit}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   )
 }
